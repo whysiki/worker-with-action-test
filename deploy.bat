52b4d2d1d74428d0b7c3dd74ab012be45aff1452
whysiki <whysiki@proton.me>
2024-09-02 04:16:53 +0800

    Reset

--- deploy.bat
+++ deploy.bat
@@ -3,7 +3,7 @@ python webhook.py https://tgbot-whysiki.oooplhhhg.workers.dev
 wrangler deploy 
 
 
-git add .; if ($?) { git commit -m "Reset" }; if ($?) { git push -f }
-
+git add .; if ($?) { git commit -m "Reset" }; if ($?) { git push -u origin master --force }
+@REM git push -u origin master --force
 
 @REM python webhook.py https://tgbot-whysiki-independ-inject.oooplhhhg.workers.dev/
